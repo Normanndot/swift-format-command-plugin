930c69efe2154437e0074a824c42f4ee21e7c44f
Norman <normanndot@gmail.com>
2022-11-18 22:20:28 +0000

    Initial commit

---
+++ SwiftPluginSample/SwiftPluginSample/SwiftPluginSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPluginSampleApp.swift
+//  SwiftPluginSample
+//
+//  Created by Norman D on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPluginSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
